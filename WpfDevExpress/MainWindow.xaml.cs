8228605322535cb26918c1c91db4649f954e780f
zhaoliang <zhaoliang0624@126.com>
2015-01-15 13:36:19 +0800

    Commit Dev Express Dll
    提交DevExpress的Dll类库

--- WpfDevExpress/MainWindow.xaml.cs
+++ WpfDevExpress/MainWindow.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using DevExpress.Xpf.Ribbon;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -12,17 +13,47 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
-
+using DevExpress.Xpf.Docking;
+using DevExpress.XtraBars;
 namespace WpfDevExpress
 {
     /// <summary>
     /// MainWindow.xaml 的交互逻辑
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class MainWindow : DXRibbonWindow
     {
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        int i;
+        void addEmptyButton_Click(object sender, RoutedEventArgs e)
+        {
+            DocumentPanel panel = dockManager.DockController.AddDocumentPanel(documentContainer);
+            panel.Caption = "Empty Panel " + (i++).ToString();
+            dockManager.Activate(panel);
+        }
+        void addUserControlUriButton_Click(object sender, RoutedEventArgs e)
+        {
+            DocumentPanel panel = dockManager.DockController.AddDocumentPanel(
+                documentContainer, new Uri("/DockingDemo;component/Layouts/ChildUserControl.xaml", UriKind.Relative));
+            panel.Caption = "UserControl " + (i++).ToString();
+            dockManager.Activate(panel);
+        }
+        void addWindowUriButton_Click(object sender, RoutedEventArgs e)
+        {
+            DocumentPanel panel = dockManager.DockController.AddDocumentPanel(
+                documentContainer, new Uri("/DockingDemo;component/Layouts/ChildWindow.xaml", UriKind.Relative));
+            panel.Caption = "Window " + (i++).ToString();
+            dockManager.Activate(panel);
+        }
+        void addPageUriButton_Click(object sender, RoutedEventArgs e)
+        {
+            DocumentPanel panel = dockManager.DockController.AddDocumentPanel(
+                documentContainer, new Uri("/DockingDemo;component/Layouts/ChildPage.xaml", UriKind.Relative));
+            panel.Caption = "Page " + (i++).ToString();
+            dockManager.Activate(panel);
         }
     }
 }
